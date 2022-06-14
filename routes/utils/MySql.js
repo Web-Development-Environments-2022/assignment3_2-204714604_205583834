97615b6cf640cf7875545103c787d3e86475fc24
Daniel Deri <daniel.derri@gmail.com>
2022-06-14 17:38:35 +0300

    connection to db

--- routes/utils/MySql.js
+++ routes/utils/MySql.js
@@ -1,13 +1,13 @@
-var mysql = require('mysql');
+var mysql = require('mysql2');
 require("dotenv").config();
 
 
 const config={
 connectionLimit:4,
-  host: process.env.host,//"localhost"
-  user: process.env.user,//"root"
-  password: "pass_root@123",
-  database:"mydb"
+  host: "localhost",//"localhost"
+  user: "root",//"root"
+  password: "root",
+  database:"recipesdb"
 }
 const pool = new mysql.createPool(config);
 
